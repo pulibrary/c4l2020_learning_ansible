2dab3f56cde809ba157bb9262c5a4236d17fa7c6
Francis Kayiwa <kayiwa@princeton.edu>
2020-01-18 16:26:58 -0500

    add ansible and vim installer scripts

---
+++ centos_script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+yum install epel-release
+yum -y update
+yum -y install vim
+# yum -y install vim ansible
---
+++ ubuntu_script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+add-apt-repository ppa:ansible/ansible
+apt -y update
+apt -y upgrade
+apt -y install vim
+# apt -y install vim ansible
